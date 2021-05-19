a37a911d25c94bda68fdb1e2db243ba4a2699649
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-05-18 22:46:07 -0700

    Re-installed server.js to server side and added proxy to package.json for working heroku depoloyment.

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
 const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
+const path = require("path");
 
-// Static connectivity for heroku
+// Heroku static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Connecting with Express
-app.use(express.urlencoded({extended: true}));
+// Connection to Express
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Routes and View
 app.use(express.static("public"));
 
-// Sends all requests to React App
+// Sends requests to REACT APP
+// Defines any API routes before this runs
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-// Starts the API server
 app.listen(PORT, () => {
   console.log(
     "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
