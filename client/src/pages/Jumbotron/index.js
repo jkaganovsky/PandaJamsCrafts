176031ed350a06dfa22197e2666ab1178228d057
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-08-02 23:22:29 -0700

    Changed font size for jumbotron title and nav links, and added text shadow to nav links.

--- client/src/pages/Jumbotron/index.js
+++ client/src/pages/Jumbotron/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Jumbotron, Image } from "react-bootstrap";
-import logo from "./LogoFace.jpg"
+import logo from "./LogoFace.jpg";
 
 function Logo() {
   return (
-    <Jumbotron className="text-center" fluid>
+    <Jumbotron className="text-center" fluid >
       <Image src={logo} rounded />
+      <h4>Panda Jams Crafts</h4>
     </Jumbotron>
   )
 }
